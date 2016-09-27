b9d22c590f5ff94aba1814897cfc65bbd0b6d91c
Eduardo Souza <eduardomfds@outlook.com>
2016-09-26 23:29:45 -0300

    readnwrite adicionados

---
+++ readnwrite.c
@@ -0,0 +1,7 @@
+/*
+ *   Projeto 1 - Montador para a arquitetura do computador IAS
+ *   Nome: Eduardo Moreira Freitas de Souza
+ *   RA: 166779
+ */
+
+
---
+++ readnwrite.h
@@ -0,0 +1,12 @@
+/*
+ *   Projeto 1 - Montador para a arquitetura do computador IAS
+ *   Nome: Eduardo Moreira Freitas de Souza
+ *   RA: 166779
+ */
+
+#ifndef READNWRITE_H_INCLUDED
+#define READNWRITE_H_INCLUDED
+
+#include "readnwrite.c"
+
+#endif /*READNWRITE_H_INCLUDED*/
