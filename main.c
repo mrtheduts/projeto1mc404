4825fa2bdd1ac65d03d31f61bc0187282dc0ad0e
Eduardo Souza <eduardomfds@outlook.com>
2016-09-29 11:13:25 -0300

    Não sei.

--- main.c
+++ main.c
@@ -9,17 +9,25 @@
 #include <string.h>
 
 #include "readnwrite.h"
+#include "textnode.h"
 
 int main (int argc, char **files){
 
     FILE *in, *out;
+    char templine[65];
 
     in = fopen(files[0], "r");
-
     if(in == NULL){
 
+    printf("Oi/n");
         printf("Não foi possível abrir o arquivo %s\n", files[0]);
+//        perror("Error: ");
         return 1;
+    }
+
+    while(fgets(templine, 65, in) != NULL){
+
+
     }
 
     return 0;
